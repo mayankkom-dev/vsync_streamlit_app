2084b74845fdcc7e369421f4ead3ae201933b5f5
Mayank Kumar <mayankkom.dev@gmail.com>
2024-01-12 04:56:09 -0500

    included requirements.txt

--- flk_scrapper.py
+++ flk_scrapper.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, StaleElementReferenceException, NoSuchElementException
-from database_utils import Post
 from dotenv import load_dotenv
 import time
 import os
