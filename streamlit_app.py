082f891596c4641161454728f37e191b48e4996b
Mayank Kumar <mayankkom.dev@gmail.com>
2024-01-12 10:34:56 -0500

    ubuntu version

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,52 +1,43 @@
+import os
+import shutil
+
 import streamlit as st
-from flk_scrapper import scrape_lk
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
-import shutil, os
-
-@st.cache_resource(show_spinner=False)
-def get_webdriver_options():
-    # Set up Chrome options
-    # Set up Chrome options
-    chrome_options = Options()
-    # Run in headless mode
-    chrome_options.add_argument("--headless")
-
-    # Set the window size (optional)
-    chrome_options.add_argument("--window-size=1920x1080")
-
-    # Disable GPU (optional, might help with headless issues)
-    chrome_options.add_argument("--disable-gpu")
-
-    # Disable images to speed up page loading (optional)
-    # chrome_options.add_argument("--blink-settings=imagesEnabled=false")
-
-    # Set the User-Agent header to mimic a regular Chrome browser
-    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-
-    # Disable notifications (optional)
-    chrome_options.add_argument("--disable-notifications")
-
-    # Disable infobars (optional)
-    chrome_options.add_argument("--disable-infobars")
-
-    # Disable extensions (optional)
-    chrome_options.add_argument("--disable-extensions")
-
-    # Disable sandboxing (optional, use with caution)
-    chrome_options.add_argument("--no-sandbox")
-
-    chrome_options.add_argument("--disable-web-security")
-    chrome_options.add_argument("--ignore-certificate-errors")
-    return chrome_options
+from selenium.webdriver.support.wait import WebDriverWait
+import google_colab_selenium as gs
 
 @st.cache_resource(show_spinner=False)
 def get_logpath():
     return os.path.join(os.getcwd(), 'selenium.log')
+
+
+@st.cache_resource(show_spinner=False)
+def get_chromedriver_path():
+    return shutil.which('chromedriver')
+
+
+@st.cache_resource(show_spinner=False)
+def get_webdriver_options():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-features=NetworkService")
+    options.add_argument("--window-size=1920x1080")
+    options.add_argument("--disable-features=VizDisplayCompositor")
+    return options
+
+
+def get_webdriver_service(logpath):
+    service = Service(
+        executable_path=get_chromedriver_path(),
+        log_output=logpath,
+    )
+    return service
 
 
 def delete_selenium_log(logpath):
@@ -62,64 +53,41 @@ def show_selenium_log(logpath):
     else:
         st.warning('No log file found!')
 
-@st.cache_resource(show_spinner=False)
-def get_chromedriver_path():
-    # return shutil.which('chromedriver')
-    chrome_version = "120.0.6099.109"  # Use the actual version of your Chrome browser
-    driver_path = ChromeDriverManager(driver_version=chrome_version).install()
-    return driver_path
 
+def run_selenium(logpath):
+    name = str()
+    # with webdriver.Chrome(options=get_webdriver_options(), service=get_webdriver_service(logpath=logpath)) as driver:
+    with gs.Chrome(options=get_webdriver_options(), keep_alive=True) as driver:        
+        # driver.manager.service.log_output = logpath
+        url = "https://www.unibet.fr/sport/football/europa-league/europa-league-matchs"
+        driver.get(url)
+        xpath = '//*[@class="ui-mainview-block eventpath-wrapper"]'
+        # Wait for the element to be rendered:
+        element = WebDriverWait(driver, 10).until(lambda x: x.find_elements(by=By.XPATH, value=xpath))
+        name = element[0].get_property('attributes')[0]['name']
+    return name
 
-def get_webdriver_service(logpath):
-    service = Service(
-        executable_path=get_chromedriver_path(),
-        log_output=logpath,
-    )
-    return service
-
-# @st.cache
-# def get_driver():
-    
-#     chrome_version = "92.0.4515.107"
-#     # Use webdriver_manager to install and manage ChromeDriver
-#     driver_path = ChromeDriverManager(driver_version=chrome_version).install()
-#     # Specify the path to the Chrome binary
-#     chrome_options.binary_location = driver_path
-
-#     print(driver_path)
-#     print("Web Driver created") 
-#     return webdriver.Chrome(service=Service(driver_path), options=chrome_options)
-#     # return driver
-
-# def run_selenium(logpath):
-#     name = str()
-#     with webdriver.Chrome(options=get_webdriver_options(), service=get_webdriver_service(logpath=logpath)) as driver:
-#         url = "https://www.unibet.fr/sport/football/europa-league/europa-league-matchs"
-#         driver.get(url)
-#         xpath = '//*[@class="ui-mainview-block eventpath-wrapper"]'
-#         # Wait for the element to be rendered:
-#         element = WebDriverWait(driver, 10).until(lambda x: x.find_elements(by=By.XPATH, value=xpath))
-#         name = element[0].get_property('attributes')[0]['name']
-#     return name
-
-def get_driver(logpath):
-    return webdriver.Chrome(options=get_webdriver_options(), service=get_webdriver_service(logpath=logpath))
-    
-
-# Streamlit UI
-def main():
-    st.title("VirtualSync")
-    if st.button("Load"):
-        # result = run_selenium(logpath=logpath)
-        driver = get_driver(logpath=logpath)
-        ret = scrape_lk(driver)
-        print(ret)
-        # st.info(f'Result -> {result}')
-        # st.info('Successful finished. Selenium log file is shown below...')
-        show_selenium_log(logpath=logpath)
-        
 
 if __name__ == "__main__":
     logpath=get_logpath()
     delete_selenium_log(logpath=logpath)
-    main()
+    st.set_page_config(page_title="Selenium Test", page_icon='✅',
+        initial_sidebar_state='collapsed')
+    st.title('🔨 Selenium on Streamlit Cloud')
+    st.markdown('''This app is only a very simple test for **Selenium** running on **Streamlit Cloud** runtime.<br>
+        The suggestion for this demo app came from a post on the Streamlit Community Forum.<br>
+        <https://discuss.streamlit.io/t/issue-with-selenium-on-a-streamlit-app/11563><br><br>
+        This is just a very very simple example and more a proof of concept.<br>
+        A link is called and waited for the existence of a specific class to read a specific property.
+        If there is no error message, the action was successful.
+        Afterwards the log file of chromium is read and displayed.
+        ''', unsafe_allow_html=True)
+    st.markdown('---')
+
+    st.balloons()
+    if st.button('Start Selenium run'):
+        st.warning('Selenium is running, please wait...')
+        result = run_selenium(logpath=logpath)
+        st.info(f'Result -> {result}')
+        st.info('Successful finished. Selenium log file is shown below...')
+        show_selenium_log(logpath=logpath)
---
+++ streamlit_app_bkp.py
@@ -0,0 +1,146 @@
+import streamlit as st
+from flk_scrapper import scrape_lk
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+import shutil, os
+from selenium.webdriver.chrome.service import Service as ChromeService
+
+@st.cache_resource(show_spinner=False)
+def get_webdriver_options():
+    # Set up Chrome options
+    # Set up Chrome options
+    chrome_options = Options()
+    # Run in headless mode
+    chrome_options.add_argument("--headless")
+
+    # Set the window size (optional)
+    chrome_options.add_argument("--window-size=1920x1080")
+
+    # Disable GPU (optional, might help with headless issues)
+    chrome_options.add_argument("--disable-gpu")
+
+    # Disable images to speed up page loading (optional)
+    # chrome_options.add_argument("--blink-settings=imagesEnabled=false")
+
+    # Set the User-Agent header to mimic a regular Chrome browser
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+
+    # Disable notifications (optional)
+    chrome_options.add_argument("--disable-notifications")
+
+    # Disable infobars (optional)
+    chrome_options.add_argument("--disable-infobars")
+
+    # Disable extensions (optional)
+    chrome_options.add_argument("--disable-extensions")
+
+    # Disable sandboxing (optional, use with caution)
+    chrome_options.add_argument("--no-sandbox")
+
+    chrome_options.add_argument("--disable-web-security")
+    chrome_options.add_argument("--ignore-certificate-errors")
+    return chrome_options
+
+@st.cache_resource(show_spinner=False)
+def get_logpath():
+    return os.path.join(os.getcwd(), 'selenium.log')
+
+
+def delete_selenium_log(logpath):
+    if os.path.exists(logpath):
+        os.remove(logpath)
+
+
+def show_selenium_log(logpath):
+    if os.path.exists(logpath):
+        with open(logpath) as f:
+            content = f.read()
+            st.code(body=content, language='log', line_numbers=True)
+    else:
+        st.warning('No log file found!')
+
+@st.cache_resource(show_spinner=False)
+def get_chromedriver_path():
+    # return shutil.which('chromedriver')
+    chrome_version = "120.0.6099.109"  # Use the actual version of your Chrome browser
+    driver_path = ChromeDriverManager(driver_version=chrome_version).install()
+    return driver_path
+
+
+def get_webdriver_service(logpath):
+    service = Service(
+        executable_path=get_chromedriver_path(),
+        log_output=logpath,
+    )
+    return service
+
+# @st.cache
+# def get_driver():
+    
+#     chrome_version = "92.0.4515.107"
+#     # Use webdriver_manager to install and manage ChromeDriver
+#     driver_path = ChromeDriverManager(driver_version=chrome_version).install()
+#     # Specify the path to the Chrome binary
+#     chrome_options.binary_location = driver_path
+
+#     print(driver_path)
+#     print("Web Driver created") 
+#     return webdriver.Chrome(service=Service(driver_path), options=chrome_options)
+#     # return driver
+
+# def run_selenium(logpath):
+#     name = str()
+#     with webdriver.Chrome(options=get_webdriver_options(), service=get_webdriver_service(logpath=logpath)) as driver:
+#         url = "https://www.unibet.fr/sport/football/europa-league/europa-league-matchs"
+#         driver.get(url)
+#         xpath = '//*[@class="ui-mainview-block eventpath-wrapper"]'
+#         # Wait for the element to be rendered:
+#         element = WebDriverWait(driver, 10).until(lambda x: x.find_elements(by=By.XPATH, value=xpath))
+#         name = element[0].get_property('attributes')[0]['name']
+#     return name
+
+def get_driver(logpath):
+    return webdriver.Chrome(options=get_webdriver_options(), service=get_webdriver_service(logpath=logpath))
+
+# @st.cache
+# def get_driver():
+#     # Initialize driver for Chrome
+#     webOpt = webdriver.ChromeOptions()
+#     webOpt.add_experimental_option('excludeSwitches', ['enable-logging'])
+#     webOpt.add_argument('--ignore-certificate-errors')
+#     webOpt.add_argument('--incognito')
+#     webOpt.headless = True
+#     chrome_version = "92.0.4515.107"
+#     driver_path = ChromeDriverManager(driver_version=chrome_version).install()
+# #     # Specify the path to the Chrome binary
+#     webOpt.binary_location = driver_path
+#     driver = webdriver.Chrome(ChromeDriverManager().install(), options=webOpt)
+#     # driver.get(seed)  # Start with the original link
+#     return driver
+
+
+
+def main():
+    st.title("VirtualSync")
+    if st.button("Load"):
+        # result = run_selenium(logpath=logpath)
+        # driver = get_driver(logpath=logpath)
+        # ret = scrape_lk(driver)
+        # print(ret)
+        # st.info(f'Result -> {result}')
+        # st.info('Successful finished. Selenium log file is shown below...')
+        # show_selenium_log(logpath=logpath)
+        driver = get_driver(logpath=logpath)
+        ret = scrape_lk(driver)
+        print(ret)
+
+        
+
+if __name__ == "__main__":
+    logpath=get_logpath()
+    delete_selenium_log(logpath=logpath)
+    main()
