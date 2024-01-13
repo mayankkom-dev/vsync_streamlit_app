204d6a53e67c850577795abc7f76112cb18f03d6
Mayank Kumar <mayankkom.dev@gmail.com>
2024-01-13 15:35:14 -0500

    remove pacakages and headless

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ def get_chromedriver_path():
 def get_webdriver_options():
     # options = Options()
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
