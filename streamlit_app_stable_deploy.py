907deca67056a61bf5559052e1f675e0cd8960b7
Mayank Kumar <mayankkom.dev@gmail.com>
2024-01-13 13:11:48 -0500

    checking head mode selenium

--- streamlit_app_stable_deploy.py
+++ streamlit_app_stable_deploy.py
@@ -24,7 +24,7 @@ def get_chromedriver_path():
 def get_webdriver_options():
     # options = Options()
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
@@ -90,15 +90,21 @@ def run_selenium(logpath):
 if __name__ == "__main__":
     logpath = get_logpath()
     delete_selenium_log(logpath=logpath)
-    st.set_page_config(page_title="VirtualSync", page_icon='🫡', initial_sidebar_state='collapsed')
-    st.title('VirtualSync')
-    st.markdown('''Interact with your virtual memory and find things faster and effecient
+    st.set_page_config(page_title="Selenium Test", page_icon='✅', initial_sidebar_state='collapsed')
+    st.title('🔨 Selenium on Streamlit Cloud')
+    st.markdown('''This app is only a very simple test for **Selenium** running on **Streamlit Cloud** runtime.<br>
+        The suggestion for this demo app came from a post on the Streamlit Community Forum.<br>
+        <https://discuss.streamlit.io/t/issue-with-selenium-on-a-streamlit-app/11563><br><br>
+        This is just a very very simple example and more a proof of concept.<br>
+        A link is called and waited for the existence of a specific class to read a specific property.
+        If there is no error message, the action was successful.
+        Afterwards the log file of chromium is read and displayed.
         ''', unsafe_allow_html=True)
     st.markdown('---')
 
     st.balloons()
-    if st.button('ReSync'):
-        st.warning('Selenium-driver loading up, please wait...')
+    if st.button('Start Selenium run'):
+        st.warning('Selenium is running, please wait...')
         result = run_selenium(logpath=logpath)
         st.info(f'Result -> {result}')
         st.info('Successful finished. Selenium log file is shown below...')
