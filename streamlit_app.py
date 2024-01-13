aa743ebbb2127588ed6da056784b8acf9b1ef48d
Mayank Kumar <mayankkom.dev@gmail.com>
2024-01-13 17:08:33 -0500

    headless mode

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ def delete_selenium_log(logpath):
 # Function to get the WebDriver options
 def get_webdriver_options():
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
