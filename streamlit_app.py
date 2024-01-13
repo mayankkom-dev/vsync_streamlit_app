b2ad2d96b0f64a707d26828e718f5b1cfecc8f81
Mayank Kumar <mayankkom.dev@gmail.com>
2024-01-12 20:02:11 -0500

    fixed import

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from exceptiongroup import TimeoutException
+from selenium.common.exceptions import TimeoutException
 
 
 @st.cache(show_spinner=False)
