324aa1ea10c1f7387f86d6c5770304cce8b8599a
Mayank Kumar <mayankkom.dev@gmail.com>
2024-01-13 19:04:49 -0500

    streamlit community test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
-import os
+import os, time
 import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 # from app_utils import *
 
 # Function to get the log path
@@ -54,28 +55,54 @@ def get_or_create_driver():
     return st.session_state.driver
 
 # Function to run sync-up logic
-def run_syncup_logic(driver):
+def run_syncup_logic(driver, usr, pwd, sync_status):
     ret = None
-    driver.get('https://www.linkedin.com')
+    login_to_linkedin(usr, pwd, driver, sync_status)
+    time.sleep(2)
+    driver.get("https://www.linkedin.com/my-items/saved-posts/")
     ret = driver.page_source
     return ret
 
-def cache_safe_resync_saved_post():
+def cache_safe_resync_saved_post(usr, pwd, sync_status):
     try:
-        resync_saved_post()
+        resync_saved_post(usr, pwd, sync_status)
     except:
         # restart Chrome driver
         st.warning("Corrupt Driver.. Restarting")
         if 'driver' in st.session_state: del st.session_state['driver']
-        resync_saved_post()
+        resync_saved_post(usr, pwd, sync_status)
 
 # Function to handle resync button click
-def resync_saved_post():
+def resync_saved_post(usr, pwd, sync_status):
     # Display a spinner while the function is running
     with st.spinner("Syncing up..."):
         driver = get_or_create_driver()
-        result = run_syncup_logic(driver)
+        sync_status.info("Got the driver")
+        result = run_syncup_logic(driver, usr, pwd, sync_status)
         st.session_state.resync_values = {'result': result}
+
+def login_to_linkedin(username, password, driver, sync_status):
+    driver.get("https://www.linkedin.com/?original_referer=")
+    time.sleep(3)
+    # find username/email field and send the username itself to the input field
+    driver.find_element("name", "session_key").send_keys(username)
+    # find password input field and insert password as well
+    driver.find_element("name", "session_password").send_keys(password)
+
+    # click login button
+    sign_in_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Sign in')]")
+    # Click the "Sign in" button
+    time.sleep(2)
+    sign_in_button.click()
+    print(f"Loged In to account : {username}")
+    sync_status.info('Successfully Logged In')
+
+
+def colapse_expander():
+    st.session_state.expander_state = False
+    st.session_state.usrname = ""
+    st.session_state.pwd = ""
+    
 
 # Main flow of the Streamlit app
 def main_flow():
@@ -87,16 +114,28 @@ def main_flow():
     # add ballon to session
     if 'first_start' not in st.session_state: st.session_state.first_start = {'value': None}
     if st.session_state.first_start['value'] is None: st.balloons()
+    
     if 'resync_values' not in st.session_state: st.session_state.resync_values = {'result': None}
-    st.button('ReSync', on_click=cache_safe_resync_saved_post)
-   
+    if "expander_state" not in st.session_state: st.session_state.expander_state = False
+    
+    with st.expander("SyncUp LinkedIn", expanded=st.session_state.expander_state):
+        st.button("Close", on_click=colapse_expander)
+        col1, col2 = st.columns(2)
+        username = col1.text_input("username",  key='usrname')
+        pwd = col2.text_input("password",  key='pwd') # type="password"
+        if username or pwd: st.session_state.expander_state = True
+        sync_status = st.empty()
+        st.button('ReSync', args=(username, pwd, sync_status), on_click=cache_safe_resync_saved_post)
+
     result_container = st.empty()
     log_container = st.empty()
     
     result = st.session_state.resync_values['result']
     if result is not None:
         with result_container:
+          sync_status.info('Sync Completed Successfully')
           st.write(result)
+          
     if st.session_state.first_start['value'] is not None:
         with log_container:  
             show_selenium_log(logpath=logpath)
