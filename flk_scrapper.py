a04c682c30c55a6e8d98307c8e47c17235b0cf1c
Mayank Kumar <mayankkom.dev@gmail.com>
2024-01-17 00:06:58 -0500

    debug msg

--- flk_scrapper.py
+++ flk_scrapper.py
@@ -23,6 +23,8 @@ password = "" #os.getenv("LINKEDIN_PASS")
 
 def scrape_lk(driver, sync_status):
     driver.get("https://www.linkedin.com/my-items/saved-posts/")
+    sync_status.write(driver.page_source)
+    time.sleep(2)
     stop_sync_flag = {'flag': False}
     db_client = RestDB()
     all_saved_items = [saved_items['id_'] for saved_items in db_client.fetch_all_items()]
