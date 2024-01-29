9c076d6d6a21bad3f570192b335a49150205dfb6
Mayank Kumar <mayankkom.dev@gmail.com>
2024-01-29 10:27:53 -0500

    enable wait for image captcha iframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ def delete_selenium_log(logpath):
 # Function to get the WebDriver options
 def get_webdriver_options():
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
     options.add_argument("--remote-debugging-pipe")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
@@ -123,17 +123,6 @@ def login_to_linkedin(username, password, driver, sync_status):
     time.sleep(2)
     # sync_status.write(driver.page_source)
     # Wait for the page_source to be loaded
-    try:
-        WebDriverWait(driver, 30).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "main-ctn"))
-            )
-        sync_status.write("Page source loaded successfully.")
-        time.sleep(10)
-        sync_status.write(driver.page_source)
-        time.sleep(10)
-    except Exception as e:
-        sync_status.write(f"Page source load failed: {e}")
-        # time.sleep(10)
     
     # check if on verification page authenticator page
     if 'Security Verification' in driver.title:
@@ -157,14 +146,15 @@ def login_to_linkedin(username, password, driver, sync_status):
         else:
             st.session_state.sync_status = "On Verification page"
             sync_status.info(st.session_state.sync_status)
-            # time.sleep(20)
+            time.sleep(10)
             # sync_status.write(driver.page_source)
             # time.sleep(10)
-            # WebDriverWait(driver, 30).until(
-            #     EC.presence_of_element_located((By.CLASS_NAME, "main-ctn"))
-            # )
-            # sync_status.write(driver.page_source)
-            # time.sleep(10)
+            WebDriverWait(driver, 100).until(
+                EC.presence_of_element_located((By.ID, "arkoseframe"))
+            )
+            st.session_state.sync_status = "found iframe"
+            sync_status.info(st.session_state.sync_status)
+            time.sleep(10)
             verification_btn = driver.find_element(By.XPATH, "//button[contains(text(), 'Verify')]")
             verification_btn.click()
             st.session_state.sync_status = "Clicked Verify"
