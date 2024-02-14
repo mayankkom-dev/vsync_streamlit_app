6d8fcd73d629c037688be610f5e550344b323e10
Mayank Kumar <mayankkom.dev@gmail.com>
2024-02-13 23:49:31 -0500

    rename duplicated module

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,10 +1,10 @@
-module "lambda_module" {
+module "lambda_module_flash" {
   source             = "./flash_lambda"
   site_packages_path = var.site_packages_path
   #   s3_bucket_name = "my-s3-bucket"
 }
 
-module "lambda_module" {
+module "lambda_module_fast" {
   source             = "./fast_lambda"
   site_packages_path = var.site_packages_path2
   #   s3_bucket_name = "my-s3-bucket"
