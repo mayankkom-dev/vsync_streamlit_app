d4a906315019e686218fe93099745527fce40f3f
Mayank Kumar <mayankkom.dev@gmail.com>
2024-02-14 00:49:22 -0500

    updated duplicated iam role policy name

--- infrastructure/flash_lambda/role_policy.tf
+++ infrastructure/flash_lambda/role_policy.tf
@@ -1,11 +1,11 @@
 resource "aws_iam_role" "vysnc_lambda_role" {
- name   = "terraform_aws_lambda_role"
+ name   = "terraform_aws_lambda_fast_role"
  assume_role_policy = file("${path.module}/policies/aws_lambda_iam_role.json")
 }
 
 # IAM policy for logging from a lambda
 resource "aws_iam_policy" "vsync_iam_policy_for_lambda" {
-  name         = "aws_iam_policy_for_terraform_aws_lambda_role"
+  name         = "aws_iam_policy_for_terraform_aws_lambda_fast_role"
   path         = "/"
   description  = "AWS IAM Policy for managing aws lambda role"
   policy = file("${path.module}/policies/aws_lambda_iam_policy.json")
