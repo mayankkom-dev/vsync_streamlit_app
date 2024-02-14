9da42f8c66f5a8b328cd3e6dce86139b657ccdfc
Mayank Kumar <mayankkom.dev@gmail.com>
2024-02-13 23:57:36 -0500

    fix fast_lambda_layer var name

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,6 +6,6 @@ module "lambda_module_flash" {
 
 module "lambda_module_fast" {
   source             = "./fast_lambda"
-  site_packages_path = var.site_packages_path2
+  site_packages_path2 = var.site_packages_path2
   #   s3_bucket_name = "my-s3-bucket"
 }
